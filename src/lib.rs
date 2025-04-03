7e189e84d8f9f1a7d8a03e410a14dc51a7084c07
KD <kundeng94@gmail.com>
2025-04-03 16:26:36 +0000

    Adding code to use test database when in debug mode (#10)
    Reviewed-on: https://git.kundeng.us/phoenix/icarus_auth/pulls/10
    Co-authored-by: KD <kundeng94@gmail.com>
    Co-committed-by: KD <kundeng94@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,13 @@ mod keys {
     pub mod error {
         pub const ERROR: &str = "DATABASE_URL must be set in .env";
     }
+
+    pub mod test {
+        pub const DBURL: &str = "TEST_DATABASE_URL";
+        pub mod error {
+            pub const ERROR: &str = "TEST_DATABASE_URL must be set in .env";
+        }
+    }
 }
 
 mod connection_settings {
@@ -22,12 +29,23 @@ pub mod db_pool {
     use crate::{connection_settings, keys};
 
     pub async fn create_pool() -> Result<sqlx::PgPool, sqlx::Error> {
-        dotenv::dotenv().ok();
-        let database_url = env::var(keys::DBURL).expect(keys::error::ERROR);
+        let database_url = get_db_url().await;
+        println!("Database url: {:?}", database_url);
 
         PgPoolOptions::new()
             .max_connections(connection_settings::MAXCONN)
             .connect(&database_url)
             .await
+    }
+
+    async fn get_db_url() -> String {
+        #[cfg(debug_assertions)] // Example: Only load .env in debug builds
+        dotenvy::dotenv().ok();
+
+        if cfg!(debug_assertions) {
+            env::var(keys::test::DBURL).expect(keys::test::error::ERROR)
+        } else {
+            env::var(keys::DBURL).expect(keys::error::ERROR)
+        }
     }
 }
