a58b0cb40b69ca44a3f0d251c90412a94c287e54
phoenix <kundeng00@pm.me>
2025-04-07 17:35:47 +0000

    Changes to token (#21)
    Reviewed-on: https://git.kundeng.us/phoenix/icarus_auth/pulls/21
    Co-authored-by: phoenix <kundeng00@pm.me>
    Co-committed-by: phoenix <kundeng00@pm.me>

--- src/callers/login.rs
+++ src/callers/login.rs
@@ -42,55 +42,48 @@ pub mod endpoint {
         axum::Extension(pool): axum::Extension<sqlx::PgPool>,
         Json(payload): Json<request::Request>,
     ) -> (StatusCode, Json<response::Response>) {
-        let usr = icarus_models::user::User {
-            username: payload.username,
-            password: payload.password,
-            ..Default::default()
-        };
-
         // Check if user exists
-        match repo::user::exists(&pool, &usr.username).await {
-            Ok(exists) => {
-                if !exists {
-                    return not_found("Not Found").await;
-                }
-            }
-            Err(err) => {
-                return not_found(&err.to_string()).await;
-            }
-        };
+        match repo::user::get(&pool, &payload.username).await {
+            Ok(user) => {
+                let salt = repo::salt::get(&pool, &user.salt_id).await.unwrap();
+                let salt_str = hashing::get_salt(&salt.salt).unwrap();
+                let unhashed_password = payload.password;
 
-        let user = repo::user::get(&pool, &usr.username).await.unwrap();
-        let salt = repo::salt::get(&pool, &user.salt_id).await.unwrap();
-        let salt_str = hashing::get_salt(&salt.salt).unwrap();
+                // Check if password is correct
+                match hashing::hash_password(&unhashed_password, &salt_str) {
+                    Ok(hash_password) => {
+                        if hashing::verify_password(&unhashed_password, hash_password.clone())
+                            .unwrap()
+                        {
+                            // Create token
+                            let key = token_stuff::get_key().unwrap();
+                            let (token_literal, duration) =
+                                token_stuff::create_token(&key).unwrap();
 
-        // Check if password is correct
-        match hashing::hash_password(&usr.password, &salt_str) {
-            Ok(hash_password) => {
-                if hashing::verify_password(&usr.password, hash_password.clone()).unwrap() {
-                    // Create token
-                    let key = token_stuff::get_key().unwrap();
-                    let (token_literal, duration) = token_stuff::create_token(&key).unwrap();
-
-                    if token_stuff::verify_token(&key, &token_literal) {
-                        (
-                            StatusCode::OK,
-                            Json(response::Response {
-                                message: String::from("Successful"),
-                                data: vec![icarus_models::login_result::LoginResult {
-                                    id: user.id,
-                                    username: user.username,
-                                    token: token_literal,
-                                    token_type: String::from(token_stuff::TOKENTYPE),
-                                    expiration: duration,
-                                }],
-                            }),
-                        )
-                    } else {
-                        return not_found("Could not verify password").await;
+                            if token_stuff::verify_token(&key, &token_literal) {
+                                (
+                                    StatusCode::OK,
+                                    Json(response::Response {
+                                        message: String::from("Successful"),
+                                        data: vec![icarus_models::login_result::LoginResult {
+                                            id: user.id,
+                                            username: user.username,
+                                            token: token_literal,
+                                            token_type: String::from(token_stuff::TOKENTYPE),
+                                            expiration: duration,
+                                        }],
+                                    }),
+                                )
+                            } else {
+                                return not_found("Could not verify password").await;
+                            }
+                        } else {
+                            return not_found("Error Hashing").await;
+                        }
                     }
-                } else {
-                    return not_found("Error Hashing").await;
+                    Err(err) => {
+                        return not_found(&err.to_string()).await;
+                    }
                 }
             }
             Err(err) => {
